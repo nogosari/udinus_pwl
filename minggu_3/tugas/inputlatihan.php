4fb4bd6730d221d58db0ba56d5043e27d85854cf
nogosari <littlebadboy1945@gmail.com>
2021-03-20 11:39:39 +0700

    update

---
+++ minggu_3/tugas/inputlatihan.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+<title> FORM MAHASISWA </title>
+</head>
+
+<body>
+    Form Mahasiswa
+    <form action="proclatihan.php" method="post" name="input">
+        Nama : <input type="text" name="nama">
+        <br>
+        <br>
+        <input type="submit" name="input" value="input">
+    </form>
+</body>
+
+
+
+
+</html>
