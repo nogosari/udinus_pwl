0111a8960390f6c88f282eaacd41e5021828bfe9
nogosari <littlebadboy1945@gmail.com>
2021-03-20 11:39:19 +0700

    update

---
+++ minggu_3/praktikum/inputtext.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Pengolahan Form ~ Text</title>
+</head>
+
+<body>
+    <formaction="prosestext.php" method="post" name="input">
+        Nama Anggota 
+        <br>
+        <input type="text" name="nama1">
+        <br>
+        <input type="text" name="nama2">
+        <br>
+        <input type="text" name="nama3">
+        <br>
+        <input type="text" name="nama4">
+        <br>
+        <input type="submit"name="input" value="input">
+    </form>
+</body>
+
+</html>
